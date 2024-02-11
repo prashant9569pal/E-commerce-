87f76813c138ce0065a266e962c1a6054730a4e0
PRASHANT PAL <118741353+prashant9569pal@users.noreply.github.com>
2024-02-11 22:32:34 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">
+              Made with ❤️ by{" "}
+              <a
+                href="https://portfolio-git-main-prashant9569pals-projects.vercel.app/"
+                className="text-decoration-underline text-dark fs-5"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Prashant Pal
+              </a>
+            </p>
+            <a
+              className="text-dark fs-4"
+              href="https://github.com/prashant9569pal"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
